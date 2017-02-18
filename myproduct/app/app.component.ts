967b7847107223906c92df575c753a9460bf06bd
vkandula <vkandula@opteamix.com>
2017-02-18 18:33:32 +0530

    Create Sample Angular app

---
+++ myproduct/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'my-product',
+    template :`<h1>Hello App</h1>`
+})
+export class AppComponent{
+    constructor() {
+
+    }
+}
